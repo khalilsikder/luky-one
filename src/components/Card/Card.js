7c47bb995840d6c92da37d0025e617d61bcc2a3b
khalilsikder <khalil.sikder03@gmail.com>
2022-03-26 17:36:17 +0600

    change

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,12 +1,12 @@
 import React from 'react';
-
+import './Card.css'
 const Card = ({data}) => {
     const {name,img,price}= data;
     console.log(data);
     return (
         <div className='card'>
             <div className='img-container'>
-                <img src={img} alt="" />
+            <img src={img} alt=''></img>
             </div>
             <h1>{name}</h1>
             <p>price :{price}</p>
